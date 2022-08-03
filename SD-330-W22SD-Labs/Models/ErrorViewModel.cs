9cc324cc6f2df854af62c56daca4555adc35da83
Jaesung Hong <jezsung@gmail.com>
2022-08-02 22:28:21 -0500

    Initial commit

---
+++ SD-330-W22SD-Labs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SD_330_W22SD_Labs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
